9380a12bc609c9ffcf51b548fd315d1e6ed7d204
Rebequis <rebeca.sousa@ufpi.edu.br>
2026-05-18 18:24:11 -0300

    Menu

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import os
 def limpar_tela():
 
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def Menu():
+    print("""
+========================= MENU =======================
+ 1-> Cadastrar produto
+ 2-> Listar Produtos
+ 3-> Atualizar Quantidade dos Produtos
+ 4-> Remover Produto
+ 5-> Consultar Estoque
+ 6-> Sair
+------------------------------------------------------""")
